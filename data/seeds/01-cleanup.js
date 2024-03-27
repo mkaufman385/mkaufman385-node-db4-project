797d0a44b41f85d4bfb17a412db0e627767cf506
mkaufman385 <mkaufman385@gmail.com>
2024-03-27 16:47:57 -0400

    wip

--- data/seeds/01-cleanup.js
+++ data/seeds/01-cleanup.js
@@ -1,1 +1,8 @@
-exports.seed = function (knex) {};
+const { clean } = require("knex-cleaner");
+
+exports.seed = function (knex) {
+  return clean(knex, {
+    mode: "truncate",
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
+  });
+};
--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -1,8 +1,1 @@
-const { clean } = require("knex-cleaner");
-
-exports.seed = function (knex) {
-  return clean(knex, {
-    mode: "truncate",
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
-  });
-};
+exports.seed = function (knex) {};
